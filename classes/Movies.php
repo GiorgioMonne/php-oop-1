7d30b21126d62f33bb9bf7c2a1a7aea8e05f21e0
Giorgio <1999giorgio.monne@mail.com>
2022-01-31 09:03:31 -1000

    creazione oggetti

--- classes/Movies.php
+++ classes/Movies.php
@@ -6,4 +6,13 @@ class Movies{
     public $anno;
     public $genere;
     public $casaproduct;
+
+    public function __construct($_nome,$_anno,$_autore,$_casaproduct,$_genere)
+    {
+        $this->nome = $_nome;
+        $this->anno = $_anno;
+        $this->autore = $_autore;
+        $this->genere = $_genere;
+        $this->casaproduct = $_casaproduct;
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
 require_once __DIR__ . '/classes/Movies.php';
+
+// $movie_1 = new Movies("spider-man","Sam_Raimi","2002","supereroi","Sony");
+
+$movies = [
+    new Movies("spider-man","Sam_Raimi","2002","supereroi","Sony"),
+    new Movies("iron-man","Jon-favreau","2008","supereroi","marvel-studios"),
+    new Movies("doctor_strange","scott-derrickson","2016","supereroi","arvel-studios"),
+    
+];

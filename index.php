4b624c6e5df84edc7d4f3dcaa9a9aa4956c5ab0b
Giorgio <1999giorgio.monne@mail.com>
2022-01-31 09:11:36 -1000

    fine stampa

--- index.php
+++ index.php
@@ -10,3 +10,33 @@ $movies = [
     new Movies("doctor_strange","scott-derrickson","2016","supereroi","arvel-studios"),
     
 ];
+
+?>
+
+<!-- var_dump($movies); -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+<body>
+
+    <h1>Lista Film</h1>
+
+    <?php foreach($movies as $movie){?>
+        <div>
+            <h2><?php echo $movie->nome ?></h2>
+            <h2><?php echo $movie->autore ?></h2>
+            <h2><?php echo $movie->anno ?></h2>
+            <h2><?php echo $movie->genere ?></h2>
+            <h2><?php echo $movie->casaproduct ?></h2>
+            <br>
+        </div>
+    <?php } ?>
+    
+</body>
+</html>
